dbc0fbc34116f0bb627ccdc6c268e8558d5227fc
Erik Faye-Lund <kusmabite@gmail.com>
2010-11-02 10:43:39 +0100

    code style

--- src/engine/effect.cpp
+++ src/engine/effect.cpp
@@ -7,8 +7,7 @@
 
 using renderer::Device;
 
-namespace engine
-{
+namespace engine {
 	Effect *loadEffect(Device &device, std::string filename)
 	{
 		ID3DXEffect *eff = NULL;
@@ -20,7 +19,7 @@ namespace engine
 #endif
 
 		HRESULT hr = D3DXCreateEffectFromFile(device, filename.c_str(), NULL, NULL, 0, NULL, &eff, &err_buf);
-		
+
 		if (FAILED(hr)) {
 			if (NULL == err_buf)
 				throw core::FatalException(std::string("failed to load effect \"") + filename + std::string("\"\n\n") + core::d3dGetError(hr));
