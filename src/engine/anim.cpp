71c7e9cb887925b421c20cbc57bf88fb8f56663d
Erik Faye-Lund <kusmabite@gmail.com>
2014-11-08 12:25:55 +0100

    no anim-mips, and error if out of memory

--- src/engine/anim.cpp
+++ src/engine/anim.cpp
@@ -10,14 +10,18 @@ engine::Anim engine::loadAnim(renderer::Device &device, std::string folder)
 		char temp[256];
 		sprintf(temp, "%s/%04d.dds", folder.c_str(), i);
 		renderer::Texture tex;
-		if (FAILED(D3DXCreateTextureFromFile(device, temp, &tex.tex))) {
+		HRESULT hr = D3DXCreateTextureFromFileEx(device, temp, D3DX_DEFAULT, D3DX_DEFAULT, D3DX_FROM_FILE, 0, D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, &tex.tex);
+		if (hr == D3DXERR_INVALIDDATA) {
 			sprintf(temp, "%s/%04d.png", folder.c_str(), i);
-			if (FAILED(D3DXCreateTextureFromFile(device, temp, &tex.tex))) {
+			hr = D3DXCreateTextureFromFileEx(device, temp, D3DX_DEFAULT, D3DX_DEFAULT, D3DX_FROM_FILE, 0, D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, &tex.tex);
+			if (hr == D3DXERR_INVALIDDATA) {
 				sprintf(temp, "%s/%04d.jpg", folder.c_str(), i);
-				if (FAILED(D3DXCreateTextureFromFile(device, temp, &tex.tex)))
+				hr = D3DXCreateTextureFromFileEx(device, temp, D3DX_DEFAULT, D3DX_DEFAULT, D3DX_FROM_FILE, 0, D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL, &tex.tex);
+				if (hr == D3DXERR_INVALIDDATA)
 					break;
 			}
 		}
+		core::d3dErr(hr);
 		anim.addTexture(tex);
 	}
 	
