2c800dfdad26c56ccc323153fd35a7fadf0a0124
Erik Faye-Lund <kusmabite@gmail.com>
2015-09-28 20:30:55 +0200

    switch from tinyxml to tinyxml2
    And use it directly from source.

--- src/scenegraph/sceneloader.cpp
+++ src/scenegraph/sceneloader.cpp
@@ -12,29 +12,30 @@
 
 #include "../math/vector3.h"
 
-#include "tinyxml.h"
+#include "tinyxml2.h"
 
 using namespace scenegraph;
+using namespace tinyxml2;
 using math::Vector3;
 using math::Quaternion;
 
-float loadFloat(TiXmlElement *elem, const char *name, float default_val)
+float loadFloat(XMLElement *elem, const char *name, float default_val)
 {
 	const char *ret = elem->Attribute(name);
 	if (!ret) return default_val;
 	return float(atof(ret));
 }
 
-static std::string loadString(TiXmlElement *elem, const char *name, const std::string &defaultString = "")
+static std::string loadString(XMLElement *elem, const char *name, const std::string &defaultString = "")
 {
 	const char *ret = elem->Attribute(name);
 	if (NULL == ret) return defaultString;
 	return ret;
 }
 
-static math::Vector3 loadVector3(TiXmlNode* node)
+static math::Vector3 loadVector3(XMLNode *node)
 {
-	TiXmlElement *elem = node->ToElement();
+	XMLElement *elem = node->ToElement();
 	assert(NULL != elem);
 	
 	return math::Vector3(
@@ -74,7 +75,7 @@ public:
 		basename(getPathName(filename))
 	{}
 
-	Node *loadPrsTransform(renderer::Device &device, TiXmlElement *xmlElem)
+	Node *loadPrsTransform(renderer::Device &device, XMLElement *xmlElem)
 	{
 		PrsTransform *ret = new PrsTransform(loadString(xmlElem, "name"));
 		
@@ -115,51 +116,51 @@ public:
 			fp = NULL;
 		}
 		
-		TiXmlNode* curr = xmlElem->FirstChild();
-		while (curr)
-		{
-			if (curr->Type() == TiXmlNode::ELEMENT)
-			{
-				TiXmlElement *currElem = curr->ToElement();
-				assert(NULL != currElem);
-				
+		XMLNode *curr = xmlElem->FirstChild();
+		while (curr) {
+			XMLElement *currElem = curr->ToElement();
+			if (currElem) {
 				const char *val = curr->Value();
-				if      (strcmp(val, "position") == 0) ret->setPosition(loadVector3(currElem));
-				else if (strcmp(val, "rotation") == 0) ret->setRotation(vector3ToQuaternion(loadVector3(currElem) * float(M_PI / 180)));
-				else if (strcmp(val, "scale")    == 0) ret->setScale(loadVector3(currElem));
-				else if (strcmp(val, "children") == 0) loadChildren(device, ret, currElem);
-				else throw std::string("unknown element \"") + val + std::string("\"");
+				if      (strcmp(val, "position") == 0)
+					ret->setPosition(loadVector3(currElem));
+				else if (strcmp(val, "rotation") == 0)
+					ret->setRotation(vector3ToQuaternion(loadVector3(currElem) * float(M_PI / 180)));
+				else if (strcmp(val, "scale")    == 0)
+					ret->setScale(loadVector3(currElem));
+				else if (strcmp(val, "children") == 0)
+					loadChildren(device, ret, currElem);
+				else
+					throw std::string("unknown element \"") + val + std::string("\"");
 			}
-			curr = xmlElem->IterateChildren(curr);
+			curr = curr->NextSibling();
 		}
 		
 		return ret;
 	}
 
 	std::map<scenegraph::TargetTransform*, std::string> targetmap;
-	Node *loadTargetTransform(renderer::Device &device, TiXmlElement *xmlElem)
+	Node *loadTargetTransform(renderer::Device &device, XMLElement *xmlElem)
 	{
 		TargetTransform *ret = new TargetTransform(loadString(xmlElem, "name"));
 		targetmap[ret] = loadString(xmlElem, "target");
 		
-		TiXmlNode* curr = xmlElem->FirstChild();
-		while (curr)
-		{
-			if (curr->Type() == TiXmlNode::ELEMENT)
-			{
-				TiXmlElement *currElem = curr->ToElement();
-				assert(NULL != currElem);
+		XMLNode *curr = xmlElem->FirstChild();
+		while (curr) {
+			XMLElement *currElem = curr->ToElement();
+			if (currElem) {
 				const char *val = curr->Value();
-				if (strcmp(val, "children") == 0) loadChildren(device, ret, currElem);
-				else throw std::string("unknown element \"") + val + std::string("\"");
+				if (strcmp(val, "children") == 0)
+					loadChildren(device, ret, currElem);
+				else
+					throw std::string("unknown element \"") + val + std::string("\"");
 			}
-			curr = xmlElem->IterateChildren(curr);
+			curr = curr->NextSibling();
 		}
 		
 		return ret;
 	}
 	
-	Node *loadCamera(TiXmlElement *xmlElem)
+	Node *loadCamera(XMLElement *xmlElem)
 	{
 		std::string fileName = loadString(xmlElem, "file");
 		
@@ -171,7 +172,7 @@ public:
 		return ret;
 	}
 	
-	Node *loadMesh(renderer::Device &device, TiXmlElement *xmlElem)
+	Node *loadMesh(renderer::Device &device, XMLElement *xmlElem)
 	{
 		std::string fileName = loadString(xmlElem, "file");
 		
@@ -184,28 +185,30 @@ public:
 		return ret;
 	}
 	
-	void loadChildren(renderer::Device &device, Node *graphNode, TiXmlElement *xmlElem)
+	void loadChildren(renderer::Device &device, Node *graphNode, XMLElement *xmlElem)
 	{
-		TiXmlNode* curr = xmlElem->FirstChild();
-		while (curr)
-		{
-			if (curr->Type() == TiXmlNode::ELEMENT)
-			{
-				TiXmlElement *currElem = curr->ToElement();
-				assert(NULL != currElem);
-				
+		XMLNode *curr = xmlElem->FirstChild();
+		while (curr) {
+			XMLElement *currElem = curr->ToElement();
+			if (currElem) {
 				Node *newChild = NULL;
 				const char *val = curr->Value();
-				if      (strcmp(val, "prs_transform") == 0)    newChild = loadPrsTransform(device, currElem);
-				else if (strcmp(val, "target_transform") == 0) newChild = loadTargetTransform(device, currElem);
-				else if (strcmp(val, "mesh") == 0)             newChild = loadMesh(device, currElem);
-				else if (strcmp(val, "camera") == 0)           newChild = loadCamera(currElem);
-				else throw std::string("unknown element \"") + val + std::string("\"");
-				
+
+				if      (strcmp(val, "prs_transform") == 0)
+					newChild = loadPrsTransform(device, currElem);
+				else if (strcmp(val, "target_transform") == 0)
+					newChild = loadTargetTransform(device, currElem);
+				else if (strcmp(val, "mesh") == 0)
+					newChild = loadMesh(device, currElem);
+				else if (strcmp(val, "camera") == 0)
+					newChild = loadCamera(currElem);
+				else
+					throw std::string("unknown element \"") + val + std::string("\"");
+
 				assert(NULL != newChild);
 				graphNode->addChild(newChild);
 			}
-			curr = xmlElem->IterateChildren(curr);
+			curr = curr->NextSibling();
 		}
 	}
 };
@@ -213,37 +216,28 @@ public:
 Scene *scenegraph::loadScene(renderer::Device &device, const std::string filename)
 {
 	Scene *scene = new Scene(filename);
-	TiXmlDocument doc;
+	tinyxml2::XMLDocument doc;
 	try
 	{
 		if (!doc.LoadFile(filename.c_str()))
-		{
-			if (doc.ErrorRow() > 0)
-			{
-				char temp[256];
-				_snprintf(temp, 256, "error at line %d: %s", doc.ErrorRow(), doc.ErrorDesc());
-				throw std::string(temp);
-			}
-			else throw std::string(doc.ErrorDesc());
-		}
-		
+			throw std::string(doc.GetErrorStr1());
+
 		SceneLoader sceneLoader(scene, filename);
 		
-		TiXmlElement *xmlRoot = doc.RootElement();
+		XMLElement *xmlRoot = doc.RootElement();
 		if (strcmp(xmlRoot->Value(), "scene") != 0) throw std::string("invalid root node \"") + xmlRoot->Value() + std::string("\"");
 		
-		TiXmlNode* curr = xmlRoot->FirstChild();
-		while (curr)
-		{
-			if (curr->Type() == TiXmlNode::ELEMENT)
+		XMLNode *curr = xmlRoot->FirstChild();
+		while (curr) {
+			XMLElement *currElem = curr->ToElement();
+			if (currElem)
 			{
-				TiXmlElement *currElem = curr->ToElement();
-				assert(NULL != currElem);
-				
-				if      (strcmp(curr->Value(), "children") == 0) sceneLoader.loadChildren(device, scene, currElem);
-				else throw std::string("unknown element \"") + curr->Value() + std::string("\"");
+				if      (strcmp(curr->Value(), "children") == 0)
+					sceneLoader.loadChildren(device, scene, currElem);
+				else
+					throw std::string("unknown element \"") + curr->Value() + std::string("\"");
 			}
-			curr = xmlRoot->IterateChildren(curr);
+			curr = curr->NextSibling();
 		}
 		
 		std::map<scenegraph::TargetTransform*, std::string>::iterator i;
